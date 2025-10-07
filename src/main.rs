5850768b98b81dbd56be27d417919241a2ac744c
Ali Aryani <lenesis@proton.me>
2025-10-08 00:26:44 +0330

    declare get_opt()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::io;
+
 fn main() {
+
 }
 
 fn f_to_c(temp:f64) -> f64{
@@ -6,4 +9,25 @@ fn f_to_c(temp:f64) -> f64{
 }
 fn c_to_f(temp:f64) -> f64{
     (temp * (9.0/5.0)) + 32.0
+}
+
+fn get_opt() -> u8 {
+    println!("Please choose:");
+    println!("1. Fahrenheit to Celsius");
+    println!("2. Celsius to Fahrenheit");
+    loop {
+        let mut opt = String::new();
+        io::stdin().read_line(&mut opt)
+            .expect("error occured.");
+        let opt:u8 = opt.trim().parse().expect("ENTER NUMBERS ONLY!");
+        if opt == 1 {
+            return 1;
+        } else if opt == 2 {
+            return 2;
+        } else {
+            println!("WRONG CHOISE!");
+            continue;
+        }
+    }
+    
 }
